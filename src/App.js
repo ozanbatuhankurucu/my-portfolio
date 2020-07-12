af3de72b7f5a342f9bed2333f8169348d1306589
ozanbatuhankurucu <50013838+ozanbatuhankurucu@users.noreply.github.com>
2020-07-13 01:17:30 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import About from './pages/about/About';
+import Mywork from './pages/mywork/Mywork';
+import Skills from './pages/skills/Skills';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Router>
+      <div>
+     <header>
+     <div className='header-container'>
+     <nav>
+        <div>
+          <h2 >S<span>2</span></h2>
+        </div>
+          <div>
+          <ul>
+            <li>
+              <Link className='nav-item' to="/">About</Link>
+            </li>
+            <li>
+              <Link  className='nav-item' to="/skills">Skills</Link>
+            </li>
+            <li>
+              <Link className='nav-item' to="/mywork">My Work</Link>
+            </li>
+          </ul></div>
+        </nav>
+     </div>
+     </header>
+         {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+           <main>
+           <div>
+           <Switch>
+          <Route path="/skills">
+            <Skills></Skills>
+          </Route>
+          <Route path="/mywork">
+            <Mywork></Mywork>
+          </Route>
+          <Route path="/">
+          <About></About>
+          </Route>
+        </Switch>
+           </div>
+           </main>
     </div>
+    </Router>
   );
 }
 
---
+++ src/pages/mywork/Mywork.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function Mywork(){
+    return <div>
+        <h1>Mywork Page</h1>
+    </div>
+}
+
+export default Mywork;
