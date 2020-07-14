836043610070e3bd443fd441ed6b92d7a4072b79
ozanbatuhankurucu <50013838+ozanbatuhankurucu@users.noreply.github.com>
2020-07-15 00:12:12 +0300

    about page tamamlandi

--- src/pages/mywork/Mywork.jsx
+++ src/pages/mywork/Mywork.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 
 function Mywork(){
-    return <div>
-        <h1>Mywork Page</h1>
+    return <div className='container-fluid'>
+    <div className='mywork-r d-flex align-items-center justify-content-center'>         
+     <h1> Coming Soon Page</h1>
     </div>
+</div>
 }
 
 export default Mywork;
--- src/pages/skills/Skills.jsx
+++ src/pages/skills/Skills.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
 
 function Skills(){
-    return <div>
-        <h1>Skills Page</h1>
+    return <div className='container-fluid'>
+        <div className='skills-r d-flex align-items-center justify-content-center'>
+     <h1> Coming Soon Page</h1>    
+        </div>
     </div>
 }
 
